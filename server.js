562832d63136a355dd7edddbbba6f67b32711264
Loquen Jones <lrjones416@gmail.com>
2019-10-04 14:40:20 -0700

    basic navbar and react router added

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const logger = require('morgan');
 
 const app = express();
 
+require('dotenv').config();
+require('./config/database');
+
 /**************** Middleware ******************/ 
 
 app.use(logger('dev'));
---
+++ src/pages/App/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+
+
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logo">SIT</div>
+        </header>
+        <Switch>
+          <Route>
+
+          </Route>
+          <Route>
+            
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
