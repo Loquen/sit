9117e0d75accf01c7e17baa3f3425d5ae4fba71c
Loquen Jones <lrjones416@gmail.com>
2019-10-12 12:12:48 -0700

    massaging dat into correct format for recharts to render

--- src/pages/VisualizePage/VisualizePage.jsx
+++ src/pages/VisualizePage/VisualizePage.jsx
@@ -1,4 +1,5 @@
 import React, { Component }from 'react';
+import moment from 'moment';
 import Filter from '../../components/Filter/Filter';
 import Stats from '../../components/Stats/Stats';
 import dayService from '../../utils/dayService';
@@ -8,12 +9,57 @@ class VisualizePage extends Component {
     days: []
   }
 
+  /********* L I F E C Y C L E ********/
   async componentDidMount(){
     let days = await dayService.getAllDays(this.props.user);
     console.log(days);
+
+    // We need to parse the data through a helper function
+    let yearData = this.populateYearData(days);
+
     this.setState({
-      days: days
+      days: days,
+      yearData: yearData
+    }, () => console.log(this.state.yearData));
+  }
+
+  /********* H E L P E R  F U N C T I O N S **********/
+  
+  populateYearData(daysList) {
+    let today = {
+      year: moment().year(), 
+      month: moment().month(),
+      day: moment().date()
+    };
+    let yearData = [
+      { month: 'Jan', totalTime: 0 },
+      { month: 'Feb', totalTime: 0 },
+      { month: 'Mar', totalTime: 0 },
+      { month: 'Apr', totalTime: 0 },
+      { month: 'May', totalTime: 0 },
+      { month: 'Jun', totalTime: 0 },
+      { month: 'Jul', totalTime: 0 },
+      { month: 'Aug', totalTime: 0 },
+      { month: 'Sep', totalTime: 0 },
+      { month: 'Oct', totalTime: 0 },
+      { month: 'Nov', totalTime: 0 },
+      { month: 'Dec', totalTime: 0 }
+    ]
+    // FORMAT of daysLIst
+    // date: Object { year: 2019, month: 9, day: 8 }
+    // ​​
+    // sessions: Array []
+    // ​​
+    // totalTime: 200
+
+    daysList.forEach(day => {
+      if(day.date.year === today.year){
+        yearData[day.date.month].totalTime += day.totalTime
+      }
     });
+      
+    // console.log(yearData);
+    return yearData;
   }
 
   render(){
